9969aea678829f09103de64c596343b4f650b7da
Ricardo Oliveira <100167292+veigakiko@users.noreply.github.com>
2025-01-15 06:57:22 -0300

    Update improve.py

--- improve.py
+++ improve.py
@@ -1389,23 +1389,26 @@ def login_page():
             font-size: 12px;
             color: #999;
         }
-        /* Botão de login com Gmail */
-        .gmail-login {
-            background-color: #db4437;
-            color: white;
-            border: none;
+        /* Botão de login com Google */
+        .gsi-material-button {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            background-color: white;
+            border: 1px solid #dadce0;
             border-radius: 4px;
             padding: 8px 16px;
             font-size: 0.9rem;
             font-weight: bold;
             cursor: pointer;
-            text-align: center;
-            margin-bottom: 10px;
-            display: block;
+            margin-top: 10px;
             width: 100%;
         }
-        .gmail-login:hover {
-            background-color: #c33d30;
+        .gsi-material-button:hover {
+            background-color: #f7f7f7;
+        }
+        .gsi-material-button-icon {
+            margin-right: 8px;
         }
         </style>
         """,
@@ -1434,18 +1437,35 @@ def login_page():
     with st.form("login_form", clear_on_submit=False):
         st.write("keep the beach vibes flowing!🌴🎾")
 
-        # Botão de login com Gmail
-        st.markdown(
-            "<button class='gmail-login'>Log in with Google</button>",
-            unsafe_allow_html=True
-        )
-
         username_input = st.text_input("Username")
         password_input = st.text_input("Password", type="password")
 
         col1, col2 = st.columns([1,1])
         with col1:
             btn_login = st.form_submit_button("Log in")
+
+        # Botão de login com Gmail
+        st.markdown(
+            """
+            <button class="gsi-material-button">
+              <div class="gsi-material-button-state"></div>
+              <div class="gsi-material-button-content-wrapper">
+                <div class="gsi-material-button-icon">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
+                    <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                    <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                    <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                    <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                    <path fill="none" d="M0 0h48v48H0z"></path>
+                  </svg>
+                </div>
+                <span class="gsi-material-button-contents">Sign in with Google</span>
+                <span style="display: none;">Sign in with Google</span>
+              </div>
+            </button>
+            """,
+            unsafe_allow_html=True
+        )
 
     # ---------------------------------------------------------------------
     # 4) Ação: Login
@@ -1489,6 +1509,7 @@ def login_page():
         """,
         unsafe_allow_html=True
     )
+
 
 ###############################################################################
 #                            INICIALIZAÇÃO E MAIN
