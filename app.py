f464246f4388161b59ab53c36835c23e9d4e3641
Ricardo Oliveira <100167292+veigakiko@users.noreply.github.com>
2025-02-07 23:39:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1633,7 +1633,7 @@ def powerbi_page():
     st.title("Power BI")
     st.markdown(
         '''
-        <iframe title="beachclub" width="700" height="573.5" src="https://app.powerbi.com/view?r=eyJrIjoiNmQwOWFlMGItMzQ5Yi00OWY0LTkzY2MtMmQzMmIxN2UyNDIxIiwidCI6ImJjYmY3MThmLTNjZmMtNGE3Ny05NTI0LWE2NWY5MjBkYTM1MSJ9&embedImagePlaceholder=true" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="beachclub" width="800" height="673.5" src="https://app.powerbi.com/view?r=eyJrIjoiNmQwOWFlMGItMzQ5Yi00OWY0LTkzY2MtMmQzMmIxN2UyNDIxIiwidCI6ImJjYmY3MThmLTNjZmMtNGE3Ny05NTI0LWE2NWY5MjBkYTM1MSJ9&embedImagePlaceholder=true" frameborder="0" allowFullScreen="true"></iframe>
         ''',
         unsafe_allow_html=True
     )
